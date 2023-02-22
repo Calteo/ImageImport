5f2e979846a04eacca27c3aacbed88c063f2988a
Calteo <matthias@famger.de>
2023-02-22 22:08:02 +0100

    push changes

--- chocolateley/tools/chocolateyinstall.ps1
+++ chocolateley/tools/chocolateyinstall.ps1
@@ -1,8 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$target = Join-Path $toolsDir "ImageImport.exe"
+$packageDir = Join-Path $toolsDir "package"
+
+Expand-Archive package.zip $packageDir
+
+$target = Join-Path $packageDir "ImageImport.exe"
 $shortCutFile = Join-Path $([Environment]::GetFolderPath("CommonStartMenu")) "Calteo\Image Import.lnk"
-Install-ChocolateyShortcut -shortcutFilePath $shortCutFile -targetPath $target -workDirectory $toolsDir -description "Image Import"
+Install-ChocolateyShortcut -shortcutFilePath $shortCutFile -targetPath $target -workDirectory $packageDir -description "Image Import"
 
 . $target register
